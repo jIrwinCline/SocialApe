c022b032a53d7634912765ffbfa5540f501ec8d0
jirwincline <jakobinwins@gmail.com>
2019-10-24 15:40:49 -0700

    combined requests into one api

--- functions/index.js
+++ functions/index.js
@@ -3,35 +3,38 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello world!");
-});
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('screams').get()
-    .then(data => {
-        let screams = [];
-        data.forEach(doc => {
-            screams.push(doc.data());
 
-        });
-        return res.json(screams);
-    })
-    .catch(err => console.error(err))
+
+app.get('/screams', (req, res) => {
+      admin
+        .firestore()
+        .collection("screams")
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then(data => {
+          let screams = [];
+          data.forEach(doc => {
+            screams.push({
+                screamId: doc.id,
+                body: doc.data().body,
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt
+            });
+          });
+          return res.json(screams);
+        })
+        .catch(err => console.error(err));
 })
 
-exports.createScream = functions.https.onRequest((req, res) => {
-    if(req.method !== 'POST') {
-        return res.status(400).json({ error: 'Method not allowed'});
-    }
+app.post('/scream',(req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: admin.firestore.Timestamp.fromDate(new Date().toISOString())
     };
 
     admin.firestore()
@@ -47,3 +50,4 @@ exports.createScream = functions.https.onRequest((req, res) => {
 
 });
 
+exports.api = functions.https.onRequest(app);
