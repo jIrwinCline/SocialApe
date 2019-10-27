dbc03ac9a07eaa1a0a7600e89f35b51b1854a1ca
jirwincline <jakobinwins@gmail.com>
2019-10-26 20:54:41 -0700

    discovered block of code that was supposed to be deleted fter .catch. could have been causing earlier errors. client error prints correctly now

--- functions/index.js
+++ functions/index.js
@@ -89,17 +89,21 @@ app.post('/signup', (req, res) => {
   })
   .catch(err => {
     console.error(err);
-    return res.status(500).json({ error: err.code });
+    if(err.code === 'auth/email-already-in-use') {
+      return res.status(400).json({ email: 'Email is already in use' });
+    } else {
+      return res.status(500).json({ error: err.code });
+    }
   })
-
-  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
-    .then(data => {
-        return res.status(201).json({ message: `user ${data.user.uid} signed up successfully`})
-    })
-    .catch(err => {
-        console.error(err)
-        return res.status(500).json({ error: err.code })
-    })
 })
 
+
+  // firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+  //   .then(data => {
+  //       return res.status(201).json({ message: `user ${data.user.uid} signed up successfully`})
+  //   })
+  //   .catch(err => {
+  //       console.error(err)
+  //       return res.status(500).json({ error: err.code })
+  //   })
 exports.api = functions.https.onRequest(app);
