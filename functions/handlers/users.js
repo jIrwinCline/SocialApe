c57c6e1f1566eb52d7eb2192990d52641ba56e04
jirwincline <jakobinwins@gmail.com>
2019-10-31 16:19:29 -0700

    fix comma typo

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -18,7 +18,7 @@ exports.signup = (req, res) => {
 
   const { valid, errors } = validateSignupData(newUser)
 
-    if(!valid) return res.status(400),json(errors);
+    if(!valid) return res.status(400).json(errors);
 
 // Validating
   let token, userId
