3d6cd424bdfd5b15ed5c7cf43560b2340c4dd088
jirwincline <jakobinwins@gmail.com>
2019-10-31 21:34:59 -0700

    stop non jpg pr pngs from uploading

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -105,7 +105,9 @@ exports.uploadImage = (req, res) => {
     let imageToBeUploaded = {};
 
     busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-        
+         if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+           return res.status(400).json({ error: "Wrong file type submitted" });
+         }
         //img png
         const imageExtension = filename.split('.')[filename.split('.').length - 1];
         imageFileName = `${Math.round(
